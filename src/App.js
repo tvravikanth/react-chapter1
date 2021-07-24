8189c65e120e4325002d6edb390116904938e5c9
EC2 Default User <ec2-user@ip-172-31-95-216.ec2.internal>
2021-07-24 03:40:58 +0000

    modified App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Ravi Kanth
         </a>
       </header>
     </div>
