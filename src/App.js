d6defe160dbd6436eddd439a6b18acf2f3b4ad8a
EC2 Default User <ec2-user@ip-172-31-95-216.ec2.internal>
2021-07-24 03:57:50 +0000

    Added Products.js referred Products in App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Products from './Products';
 
 class App extends Component {
    render() {
@@ -6,6 +7,7 @@ class App extends Component {
 		   <div>
 		      <h1>
 		        My First React App!
+		        <Products />
 		      </h1>
 		   </div>
 	   );
